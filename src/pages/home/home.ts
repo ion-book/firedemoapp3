57b86352fe0347cb6464e94fd9ed0ea541ac9811
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-08-14 17:34:49 -0500

    add boton + firebase JS SDK

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public myPicture: string = null;
 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public cameraPlugin: Camera) {}
+
+  takePicture(){
+    this.cameraPlugin.getPicture({
+      quality : 95,
+      destinationType : this.cameraPlugin.DestinationType.DATA_URL,
+      sourceType : this.cameraPlugin.PictureSourceType.CAMERA,
+      allowEdit : true,
+      encodingType: this.cameraPlugin.EncodingType.PNG,
+      targetWidth: 500,
+      targetHeight: 500,
+      saveToPhotoAlbum: true
+    }).then(imageData => {
+      this.myPicture = imageData;
+    }, error => {
+      console.log("ERROR -> " + JSON.stringify(error));
+    });
   }
 
 }
